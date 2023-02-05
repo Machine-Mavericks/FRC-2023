05d37ff002ec0a4d2ee34ed2265635a3d4bbf905
bambam7480 <neutrino314@outlook.com>
2023-02-05 13:29:57 -0500

    See previous comment

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -49,6 +49,8 @@ public class OI {
     private static final XboxController driverController = new XboxController(DRIVER_CONTROLLER_PORT);
     /** Controller used by driver, mapped to {@link #OPERATOR_CONTROLLER_PORT} */
     private static final XboxController operatorController = new XboxController(OPERATOR_CONTROLLER_PORT);
+    public static final JoystickButton balanceButton = new JoystickButton(operatorController, OperatorButtons.BALANCE_BUTTON.value);
+    public static final JoystickButton resetButton = new JoystickButton(operatorController, OperatorButtons.RESET_BUTTON.value);
 
     /** button for move command. Mapped to {@link DriverButtons#ROBOT_PRECISION_MOVE} */
     public static final JoystickButton PrecisionMoveButton = new JoystickButton(driverController, DriverButtons.ROBOT_PRECISION_MOVE.value);
