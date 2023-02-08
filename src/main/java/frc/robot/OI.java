ebeea82719dcb3a2e7a9d517f4dbe260ee19328d
DanDjuric <122225008+DanDjuric@users.noreply.github.com>
2023-02-07 19:47:02 -0500

    Update OI.java
    Slow the robot down

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -51,15 +51,15 @@ public class OI {
     // The sticks/controllers are kept private so that if we want to switch them later, this is the only place needing changes
     // Use buttons and DoubleSuppliers to expose any inputs you want elsewhere
     public static double getXDriveInput(){
-        return OI.driverController.getLeftX();
+        return OI.driverController.getLeftX()*0.5;
     }
 
     public static double getYDriveInput(){
-        return OI.driverController.getLeftY();
+        return OI.driverController.getLeftY()*0.5;
     }
 
     public static double getRotateDriveInput(){
-        return OI.driverController.getRightX();
+        return OI.driverController.getRightX()*0.5;
     }
 
     public static boolean getParkButton(){
