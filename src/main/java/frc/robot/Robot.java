123edb6b75c01b0cd7d8db0f66eb5ad5f74c9f23
lilyen06 <74439321+lilyen06@users.noreply.github.com>
2023-02-22 17:45:16 -0500

    testing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -115,8 +115,7 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    
-  
+    RobotContainer.NodeTargeting.setRightBottomTarget();
   }
 
   @Override
