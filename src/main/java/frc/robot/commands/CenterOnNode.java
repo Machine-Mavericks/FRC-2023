8a659690a079956baad70bdf50c41bb0f7f313b4
lilyen06 <74439321+lilyen06@users.noreply.github.com>
2023-02-28 20:40:34 -0500

    test

--- src/main/java/frc/robot/commands/CenterOnNode.java
+++ src/main/java/frc/robot/commands/CenterOnNode.java
@@ -60,18 +60,22 @@ public class CenterOnNode extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    System.out.println("----------------------------------------------------------------------------------------------------");
     driveCompensate();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ end");
     swerveDrive.drive(0.0, 0.0, 0.0, false, true);
+    System.out.println("PARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARKPARK");
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    System.out.println("*********************************************************************************************** isFin");
     return (Math.abs(tx)<=2.0);
   }
 }
