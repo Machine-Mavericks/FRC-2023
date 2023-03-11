d33b7dad1a257d51ca043e84cda945890e00d04c
KatieFardella <katie.fardella@gmail.com>
2023-03-10 20:28:32 -0500

    Skeleton for floor pickup

---
+++ src/main/java/frc/robot/commands/SemiAutonomous/FloorPickup.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.SemiAutonomous;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+import frc.robot.commands.SetArmPosition;
+import frc.robot.subsystems.Arm;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class FloorPickup extends SequentialCommandGroup {
+  /** Creates a new FloorPickup. */
+  public FloorPickup() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    
+
+     if (RobotContainer.gamepiecetargeting.isTarget()){
+        addCommands(
+          // center on target (depends on camera mount) use gamepiecetargeting.getHorizontalTargetOffset
+          new SetArmPosition(Arm.LOW_DEG),
+          new InstantCommand(() -> RobotContainer.grabber.setPosition(RobotContainer.grabber.getPosition().Open)),
+          // drive towards the cone until it reaches a  certain gamepiecetargeting.getGamepieceDistance
+          new InstantCommand(() -> RobotContainer.grabber.setPosition(RobotContainer.grabber.getPosition().Close)),
+          new SetArmPosition(Arm.STOW_DEG)
+        );
+     } else {
+        addCommands(null);
+     }
+  }
+}
